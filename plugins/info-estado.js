e2c436a3097235265b4633cde5a97a168cb20dc1
GABRIEL-OFF18 <virginiarimachipalmares@gmail.com>
2025-07-11 20:33:11 -0500

    Actualizar info-estado.js

--- plugins/info-estado.js
+++ plugins/info-estado.js
@@ -16,8 +16,8 @@ let handler = async (m, { conn, isRowner }) => {
     const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats);
     const groupsIn = chats.filter(([id]) => id.endsWith('@g.us'));
     const used = process.memoryUsage();
-    let Sisked = `╭─⬣「 *Estado De sᥲsᥙkᥱ ᑲ᥆𝗍 mძ 🌀* 」⬣\n`;
-    Sisked += `│ 👤 *Creador ∙* Barboza\n`;
+    let Sisked = `╭─⬣「 *Estado De ISAGI YOICHI ᑲ᥆𝗍 mძ * 」⬣\n`;
+    Sisked += `│ 👤 *Creador ∙* Gabriel\n`;
     Sisked += `│ 💎 *Grupos Unidos ∙* ${groupsIn.length}\n`;
     Sisked += `│ 💨 *Chats Privados ∙* ${chats.length - groupsIn.length}\n`;
     Sisked += `│ 🪙 *Total De Chats ∙* ${chats.length}\n`;

ce3ef88e8fdb530d510ea345b497ecdd0065bae2
GABRIEL-OFF18 <virginiarimachipalmares@gmail.com>
2025-07-11 20:49:01 -0500

    Actualizar main-grupos.js

--- plugins/main-grupos.js
+++ plugins/main-grupos.js
@@ -3,18 +3,19 @@ import fetch from 'node-fetch';
 let handler = async (m, { conn, usedPrefix, command }) => {
     let grupos = "*Hola!, te invito a unirte a los grupos oficiales  para convivir con la comunidad* ⭐\n\n" +
                  "1-Gabriel\n" +
-                 "*✰* https://whatsapp.com/channel/0029VbAIBneK0IBjjFDyMn31" +
+                 "*✰* https://chat.whatsapp.com/KLHc8jL5chQ2aVUxFtg0ms?mode=r_t" +
                  "*─ׄ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׄ*\n\n" +
                  "➠ Enlace anulado? entre aquí! \n\n" +
                  "⭐ Canal :\n" +
-                 "*✰*https://whatsapp.com/channel/0029Vb8kvXUBfxnzYWsbS81I" +
-                 "> By Barboza";
+                 "*✰*.
+https://whatsapp.com/channel/0029VbAIBneK0IBjjFDyMn31" +
+                 "> By Gabriel";
 
     // Asegúrate de definir 'imagen2' correctamente antes de usarlo
     let imagen2 = 'https://qu.ax/Mvhfa.jpg';
 
     // Define los emojis que quieres usar
-    let emojis = '🍁';
+    let emojis = '👾';
 
     await conn.sendFile(m.chat, imagen2, "ian.jpg", grupos, m, null, rcanal);
     await m.react(emojis);

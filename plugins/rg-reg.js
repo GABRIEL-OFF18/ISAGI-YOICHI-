4a94685aa067dfa5b1fc723a8be290b795805a5c
GABRIEL-OFF18 <virginiarimachipalmares@gmail.com>
2025-07-11 20:58:06 -0500

    Actualizar rg-reg.js

--- plugins/rg-reg.js
+++ plugins/rg-reg.js
@@ -11,7 +11,7 @@ let handler = async (m, { conn, text, usedPrefix, command}) => {
 }
 
     if (!regFormat.test(text)) {
-        return m.reply(`❌ Formato incorrecto.\nUsa: *${usedPrefix + command} Nombre.Edad.País*\nEjemplo: *${usedPrefix + command} Barboza.18.Venezuela*`);
+        return m.reply(`❌ Formato incorrecto.\nUsa: *${usedPrefix + command} Nombre.Edad.País*\nEjemplo: *${usedPrefix + command} Gabriel.18.Peru*`);
 }
 
     let [_, name, age, country] = text.match(regFormat);

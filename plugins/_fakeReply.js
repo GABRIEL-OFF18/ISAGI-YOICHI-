848432c145993d5bce4755a4861ab5c9c35f0e31
GABRIEL-OFF18 <virginiarimachipalmares@gmail.com>
2025-07-11 20:09:20 -0500

    Actualizar _fakeReply.js

--- plugins/_fakeReply.js
+++ plugins/_fakeReply.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 export async function before(m, { conn }) {
 let name = `ᥴһᥲᥒᥒᥱᥣ-sᥲsᥙkᥱ ᑲ᥆𝗍 mძ 🌀`
 let imagenes = ["https://h.uguu.se/iRbSvBAW.jpg",
-"https://files.catbox.moe/6dewf4.jpg",
+"https://files.catbox.moe/34d2w8.jpg",
 "https://files.catbox.moe/6dewf4.jpg",
 "https://files.catbox.moe/6dewf4.jpg",]
 

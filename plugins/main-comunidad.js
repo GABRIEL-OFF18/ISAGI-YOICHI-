dc7e1b57613e2e7fa2f7141d0b3e7bb30d15e57b
GABRIEL-OFF18 <virginiarimachipalmares@gmail.com>
2025-07-12 22:00:34 -0500

    Actualizar main-comunidad.js

--- plugins/main-comunidad.js
+++ plugins/main-comunidad.js
@@ -6,21 +6,21 @@ const handler = async (m, { conn }) => {
      *COMUNIDAD*  
 ──────── ⚔ ────────  
 
-*Bot barboza*  
-• ,👥➤ **Grupo de WhatsApp de la comunidad de sᥲsᥙkᥱ ᑲ᥆𝗍 mძ 🌀**  
-   Únete para compartir y resolver dudas con otros usuarios. 
-  ➤https://whatsapp.com/channel/0029Vaua0ZD3gvWjQaIpSy18
+*Bot Gabriel*  
+• ,👥➤ **Grupo de WhatsApp de la comunidad de ISAGI YOICHI ᑲ᥆𝗍 🚀**  
+   Únete para compartir y resolver problemas con otros usuarios. 
+  ➤https://chat.whatsapp.com/KLHc8jL5chQ2aVUxFtg0ms?mode=r_t
 
-• 📢 ➤ *Canal de Bot Barboza Ai*  
+• 📢 ➤ *Canal de Bot ISAGI YOICHI*  
    Recibe actualizaciones, noticias y lanzamientos del bot.  
-https://whatsapp.com/channel/0029Vaua0ZD3gvWjQaIpSy18
+https://whatsapp.com/channel/0029VbAIBneK0IBjjFDyMn31
 • 💬 ➤ *Grupo de WhatsApp activo*  
-   Chatea con usuarios en tiempo real y sé parte de la conversación y usa al bot que esta de uso libre.  
-➤https://chat.whatsapp.com/E1kx7olE0RpA18BdALdaWV
+   Chatea con usuarios en tiempo real y sé parte de la conversación y usa al bot que esta de uso.  
+➤https://chat.whatsapp.com/KLHc8jL5chQ2aVUxFtg0ms?mode=r_t
 
 ──────── ⚔ ────────  
 🔍 *¿Sabías que...?* 
-- El bot Barboza Ai es actualizado regularmente para mejorar su desempeño.  
+- El bot ISAGI YOICHI 𝐁𝐨𝐭 es actualizado regularmente para mejorar su desempeño.  
 - Puedes sugerir mejoras o reportar errores directamente en los grupos.  
 - Nuestra comunidad sigue creciendo y cuenta con soporte activo.  
 -
